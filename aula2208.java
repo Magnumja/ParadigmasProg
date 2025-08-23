bd907db1ae08d10bff9f63f229b6c2b584fbfd29
Magnumja <162545876+Magnumja@users.noreply.github.com>
2025-08-22 20:15:47 -0400

    up

---
+++ aula2208.java
@@ -0,0 +1,15 @@
+public class aula2208 {
+    public static int x = 22;
+    public static double y= 12.2;
+    public static float z = 1.8f;
+    public static boolean a = true;
+    public static String b = "Palavra";
+    public static char c = 'x';
+    public static double soma = x+y;
+
+
+    public static void main(String[] args){
+        
+        System.out.println("Soma" + " vale " + soma);
+    }
+}
