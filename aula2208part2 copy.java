91a5bff14d22763b955322a15f8110f8294e17b4
Magnumja <162545876+Magnumja@users.noreply.github.com>
2025-08-22 20:34:43 -0400

    up2

---
+++ aula2208part2 copy.java
@@ -0,0 +1,13 @@
+public class aula2208part2{
+    public static void main(String[] args) {
+        int x = 22;
+        double y = 12.2;
+        float z = 1.8f;
+        boolean a = true;
+        String b = "Palavra";
+        char c = 'x';
+        double soma = x + y;
+
+        System.out.println(soma);
+    }
+}
