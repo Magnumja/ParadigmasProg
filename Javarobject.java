d76e25d78a7ca4941aeb5d52f21b8e29b95cabc4
Magnum de Abreu <162545876+Magnumja@users.noreply.github.com>
2025-08-29 19:57:49 -0400

    Create Javarobject.java

---
+++ Javarobject.java
@@ -0,0 +1,18 @@
+public class Javarobject {
+
+  public static void main(String[] args){
+    Bebe bebe = new Bebe();
+
+    bebe.idade = 15;
+
+    system.out.println("Idade primeira vez: " + bebe.idade);
+    Scanner sc = new Scanner(System.in);
+    int y;
+    y = sc.nextInt();
+
+    bebe.valorIdade(y);
+    bebe.mostrarTudo();
+
+  }
+
+}
