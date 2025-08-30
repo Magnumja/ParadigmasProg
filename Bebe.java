d55f66b6757425b8fa8f286f8980250f98dbc54b
Magnum de Abreu <162545876+Magnumja@users.noreply.github.com>
2025-08-29 20:11:09 -0400

    Update Bebe.java

--- Bebe.java
+++ Bebe.java
@@ -1,6 +1,18 @@
 package com.mycompany.javaproject;
 
 public class Bebe {
+
+/*
+  private int idade;
+  public int getidade(){
+return idade;
+}
+
+public void setIdade(int idade){
+this.idade = idade;
+}
+  
+  */
 private int idade;
   public void valorIdade(int x){
     this.idade = x;
@@ -10,3 +22,4 @@ public void mostrarTudo() {
   System.out.println("Idade: " + idade);
 }
 }
+
