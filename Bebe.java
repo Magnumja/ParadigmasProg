a6435cc139316728f557cde84fdbd83f747ec56d
Magnum de Abreu <162545876+Magnumja@users.noreply.github.com>
2025-08-29 20:00:59 -0400

    Create Bebe.java

---
+++ Bebe.java
@@ -0,0 +1,12 @@
+package com.mycompany.javaproject;
+
+public class Bebe {
+private int idade;
+  public void valorIdade(int x){
+    this.idade = x;
+  }
+
+public void mostrarTudo() {
+  System.out.println("Idade: " + idade);
+}
+}
