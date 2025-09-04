56163ca2a568f899fe3fc2fc0660b60333066df4
Magnumja <162545876+Magnumja@users.noreply.github.com>
2025-09-03 21:14:58 -0400

    update

---
+++ trabalho1/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        trabalho1 pessoa = new trabalho1();
+
+        
+        pessoa.setNome("Magnum Johanson de Abreu");
+        pessoa.setIdade(20);
+        pessoa.setSalario(5000.00);
+
+        
+        System.out.println("Nome: " + pessoa.getNome());
+        System.out.println("Idade: " + pessoa.getIdade());
+        System.out.println("Salário: " + pessoa.getSalario());
+    }
+}
---
+++ trabalho1/trabalho1.java
@@ -0,0 +1,32 @@
+public class trabalho1 {
+    private String nome;
+    private int idade;
+    private double salario;
+
+    // Getter
+public String getNome() {
+    return nome;
+}
+
+public double getSalario() {
+    return salario;
+}
+
+public int getIdade() {
+    return idade;
+}
+
+// Setter
+public void setNome(String nome) {
+    this.nome = nome;
+}
+
+public void setIdade(int idade) {
+    this.idade = idade;
+}
+
+public void setSalario(double salario) {
+    this.salario = salario;
+}
+
+}
